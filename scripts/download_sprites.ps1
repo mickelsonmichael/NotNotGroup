374f3377c037705ff1cffb4aaca369cb2dffdf04
mickelsonmichael <mickelsonmichael@gmail.com>
2021-10-31 11:50:32 -0600

    Add image download script and initial downloads

---
+++ scripts/download_sprites.ps1
@@ -0,0 +1,21 @@
+$rootUrl = "https://oldschool.runescape.wiki"
+$outFolder = "C:/dev/NotNotGroup/web/src/skills"
+
+Write-Host "Downloading images to $outFolder"
+
+if (!(test-Path -PathType Container "$outFolder/images"))
+{
+    New-Item -Path "$outFolder/images" -ItemType Directory
+}
+
+$resp = Invoke-WebRequest $rootUrl/w/Skill_training_guides
+
+$output = @{ label="output"; expression={ "$outFolder$($_.src.split("?")[0])" } }
+$url = @{ label="url"; expression={ "$rootUrl$($_.src)" }}
+
+$resp.Images `
+    | Where-Object { $_.src -match "/images/([A-Za-z]+)_icon.png" } `
+    | Select-Object -Property src,$output,$url `
+    | ForEach-Object {
+        Invoke-WebRequest -OutFile $_.output $_.url
+    }

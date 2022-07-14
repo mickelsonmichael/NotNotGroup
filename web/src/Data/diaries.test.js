81889236e4dd17446a24397deccdef4879ac606d
mickelsonmichael <mickelsonmichael@gmail.com>
2022-07-14 15:15:55 -0600

    Add desert diaries

---
+++ web/src/Data/diaries.test.js
@@ -0,0 +1,72 @@
+import diaries from "./diaries.json";
+import { quests } from "./questData.json";
+
+const expectedPlayers = [
+    "NotNotThomas",
+    "NotNotMike"
+];
+
+const expectedSkills = [
+    "Attack",
+    "Defence",
+    "Strength",
+    "Hitpoints",
+    "Ranged",
+    "Prayer",
+    "Magic",
+    "Cooking",
+    "Woodcutting",
+    "Fletching",
+    "Fishing",
+    "Firemaking",
+    "Crafting",
+    "Smithing",
+    "Mining",
+    "Herblore",
+    "Agility",
+    "Thieving",
+    "Slayer",
+    "Farming",
+    "Runecraft",
+    "Hunter",
+    "Construction"
+];
+
+describe("Diaries list", () => {
+    test("all skills are valid", () => {
+        Object.keys(diaries)
+            .flatMap(region => Object.keys(diaries[region]).flatMap(difficulty => diaries[region][difficulty]))
+            .map(task => task.skills)
+            .forEach(skillMap => {
+                Object.keys(skillMap)
+                    .forEach(skillName => {
+                        const level = skillMap[skillName];
+                        expect(expectedSkills).toContain(skillName);
+                        expect(level).toBeLessThanOrEqual(99);
+                        expect(level).toBeGreaterThanOrEqual(1);
+                    })
+            });
+    });
+
+    test("all quest requirements are valid", () => {
+        const questNames = quests.map(q => q.name);
+
+        Object.keys(diaries)
+            .flatMap(region => Object.keys(diaries[region]).flatMap(difficulty => diaries[region][difficulty]))
+            .flatMap(task => task.quests)
+            .forEach(quest => {
+                expect(questNames).toContain(quest);
+            });
+    });
+
+    test("all players valid", () => {
+        Object.keys(diaries)
+            .flatMap(region => Object.keys(diaries[region]).flatMap(difficulty => diaries[region][difficulty]))
+            .map(task => task.players)
+            .forEach(playerList => {
+                playerList.forEach(p => {
+                    expect(expectedPlayers).toContain(p);
+                })
+            })
+    })
+});

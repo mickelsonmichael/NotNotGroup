e578e7ba1733783799079faa4a0d5f2c91056c6d
mickelsonmichael <mickelsonmichael@gmail.com>
2022-07-19 13:35:46 -0600

    Use new google sheet for diary indicators

--- web/src/Components/DiaryIndicator/useNumberOfCompletedTasks.js
+++ web/src/Components/DiaryIndicator/useNumberOfCompletedTasks.js
@@ -1,7 +1,7 @@
 const useNumberOfCompletedTasks = (player, tasks) => {
   if (player == null) return 0;
 
-  return tasks.filter((task) => task.players.includes(player.name)).length;
+  return tasks.filter((task) => player.diaries.includes(task.description)).length;
 };
 
 export default useNumberOfCompletedTasks;

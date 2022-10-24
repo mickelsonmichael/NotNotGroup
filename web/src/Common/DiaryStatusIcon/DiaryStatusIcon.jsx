d19cc9e2a9d8c280e320ede9e4863e4994a740a7
mickelsonmichael <mickelsonmichael@gmail.com>
2022-10-24 09:15:41 -0600

    Fix logic in DiaryStatusIcon

--- web/src/Common/DiaryStatusIcon/DiaryStatusIcon.jsx
+++ web/src/Common/DiaryStatusIcon/DiaryStatusIcon.jsx
@@ -3,17 +3,32 @@ import NoIcon from "../NoIcon";
 import YesIcon from "../YesIcon";
 import WarnIcon from "../WarnIcon";
 
-const DiaryStatusIcon = ({ player, diary, mr }) =>
-  player != null && player.diaries.includes(diary.description) ? (
-    <YesIcon mr={mr} />
-  ) : player != null &&
-    Object.keys(diary.skills).every(
-      (skillName) => (player.skills[skillName]?.level ?? 0) >= diary[skillName]
-    ) &&
-    diary.quests.every((q) => player.quests.includes(q)) ? (
-    <WarnIcon mr={mr} />
-  ) : (
-    <NoIcon mr={mr} />
-  );
+const playerLevelExceedsRequirement = (skill) => skill.playerLevel >= skill.requiredLevel;
+
+const DiaryStatusIcon = ({ player, diary, mr }) => {
+  // Complete
+  if (player != null && player.diaries.includes(diary.description)) {
+    return <YesIcon mr={mr} />;
+  }
+
+  const hasEverySkill = Object.keys(diary.skills)
+    .filter((skillName) => diary.skills[skillName] !== undefined)
+    .map((skillName) => ({
+      skillName,
+      playerLevel: Number(player.skills[skillName]?.level ?? 0),
+      requiredLevel: Number(diary.skills[skillName]),
+    }))
+    .every(playerLevelExceedsRequirement);
+
+  const hasEveryQuest = diary.quests.every((q) => player.quests.includes(q));
+
+  // Has Requirements
+  if (hasEverySkill && hasEveryQuest) {
+    return <WarnIcon mr={mr} />;
+  }
+
+  // Has missing requirements
+  return <NoIcon mr={mr} />;
+};
 
 export default DiaryStatusIcon;
